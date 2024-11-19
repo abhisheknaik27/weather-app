0c426eeaee18e4a414ff8ba3f66f97ed6af8636e
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-19 13:36:07 +0530

    weather ui completed

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import './Weather.css';
+import { FaSearchLocation } from "react-icons/fa";
+import clouds from '../assets/clouds.png';
+import drizzle from '../assets/drizzle.png';
+import rain from '../assets/rain.png';
+import snowfall from '../assets/snowfall.png';
+import sun from '../assets/sun.png';
+import wind from '../assets/wind.png';
+import { WiHumidity } from "react-icons/wi";
+import { PiWindFill } from "react-icons/pi";
+
 
 const Weather = () => {
   return (
-    <div>
-      <h1>Weather</h1>
+    <div className='weather'>
+       <div className='searchbar'>
+        <input type="text" placeholder='Search' />
+        <FaSearchLocation className='searchicon' />
+       </div>
+       <img src={sun} className='weather-icon' alt="" />
+       <p className='temperature'>16°C</p>
+       <p className='location'>London</p>
+
+       <div className="weather-data">
+        <div className="col">
+            <div className='icon'>
+            <WiHumidity />
+            </div>
+            <div className='text'>
+                <p>91%</p>
+                <span>Humidity</span>
+            </div>
+        </div>
+        
+        <div className="col">   
+            <div className='icon'>
+                <PiWindFill />
+            </div>
+            <div className='text'>
+                <p>3.6 km/h</p>
+                <span>Wind Speed</span>
+            </div>
+        </div>
+       </div>
     </div>
   )
 }
